c30d7263a64988b4c8e1b4286c77a9f909fa36a2
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-06 20:49:58 -0300

    Cleaning particle.js

--- js/particle.js
+++ js/particle.js
@@ -1,16 +1,9 @@
-/* Credit and Thanks:
-Matrix - Particles.js;
-SliderJS - Ettrics;
-Design - Sara Mazal Web;
-Fonts - Google Fonts
-*/
-
 window.onload = function () {
     Particles.init({
         selector: ".background"
     });
 };
-const particles = Particles.init({
+Particles.init({
     selector: ".background",
     color: ["#03dac6", "#ff0266", "#000000"],
     connectParticles: true,
@@ -79,24 +72,25 @@ class NavigationPage {
             $(window).scrollTop() > this.lastScroll &&
             $(window).scrollTop() > offset
         ) {
-            $(".nav-container").addClass("nav-container--move-up");
-            $(".nav-container").removeClass("nav-container--top-first");
-            $(".nav-container").addClass("nav-container--top-second");
+            $(".nav-container").addClass("nav-container--move-up")
+                .removeClass("nav-container--top-first")
+                .addClass("nav-container--top-second");
+
         } else if (
             $(window).scrollTop() < this.lastScroll &&
             $(window).scrollTop() > offset
         ) {
-            $(".nav-container").removeClass("nav-container--move-up");
-            $(".nav-container").removeClass("nav-container--top-second");
+            $(".nav-container").removeClass("nav-container--move-up")
+                .removeClass("nav-container--top-second");
             $(".nav-container-container").addClass("nav-container--top-first");
         } else {
-            $(".nav-container").removeClass("nav-container--move-up");
-            $(".nav-container").removeClass("nav-container--top-first");
-            $(".nav-container").removeClass("nav-container--top-second");
+            $(".nav-container").removeClass("nav-container--move-up")
+                .removeClass("nav-container--top-first")
+                .removeClass("nav-container--top-second");
         }
     }
 
-    findCurrentTabSelector(element) {
+    findCurrentTabSelector() {
         let newCurrentId;
         let newCurrentTab;
         let self = this;
@@ -113,7 +107,7 @@ class NavigationPage {
                 newCurrentTab = $(this);
             }
         });
-        if (this.currentId != newCurrentId || this.currentId === null) {
+        if (this.currentId !== newCurrentId || this.currentId === null) {
             this.currentId = newCurrentId;
             this.currentTab = newCurrentTab;
             this.setSliderCss();
@@ -127,15 +121,10 @@ class NavigationPage {
             width = this.currentTab.css("width");
             left = this.currentTab.offset().left;
         }
-        $(".nav-tab-slider").css("width", width);
-        $(".nav-tab-slider").css("left", left);
+        $(".nav-tab-slider").css("width", width)
+            .css("left", left);
     }
 }
 
 new NavigationPage();
-/* Credit and Thanks:
-Matrix - Particles.js;
-SliderJS - Ettrics;
-Design - Sara Mazal Web;
-Fonts - Google Fonts
-*/
+
